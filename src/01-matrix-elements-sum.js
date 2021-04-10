08c43bae569c4701fb440487fe4a8dfac72d6599
MaxZay <maximzay2017@gmail.com>
2021-04-10 15:17:53 +0300

    feat: solve 01-matrix-elements-sum.js

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,32 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  const nullX = [];
+  const nullY = [];
+  let flag = false;
+  let result = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      flag = true;
+      if (matrix[i][j] === 0) {
+        nullX.push(j);
+        nullY.push(i);
+      }
+      if (matrix[i][j] !== 0) {
+        for (let l = 0; l < nullX.length; l++) {
+          if (i > nullY[l] && j === nullX[l]) {
+            flag = false;
+            break;
+          }
+        }
+        if (flag === true) {
+          result += matrix[i][j];
+        }
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = getMatrixElementsSum;
